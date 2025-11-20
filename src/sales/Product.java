c3a296617575879fea2a694b4644609f3fc23efd
inmiakoh <akohinmi90@gmail.com>
2025-11-20 21:51:41 +0000

    Tst

--- src/sales/Product.java
+++ src/sales/Product.java
@@ -1,6 +1,6 @@
 package sales;
 
-public class Product implements Saleable{
+public class Product implements Saleable{ //product provides the getPrice method from that interface 
 	private int price;
 	private int weight;
 	
@@ -9,21 +9,25 @@ public class Product implements Saleable{
 		this.weight = weight;
 	}
 	
-	@Override
+	//because the method comes from saleable.
+	@Override 
 	public int getPrice() {
 		return price;
 	}
 	
 	public int calculateDelivery() {
+		//if the weight is less than 100g then the delivery is free
 		if (weight < 100) {
 			return 0;
+		//if the weight is between 100 and 999g then the delivery cost is 20% of the weight 
 		} else if (weight <1000) {
 			return(int)(weight * 0.2); 
+		//if the weight is 1000g or more the first 1000g would cost 20% then the rest after would be 10%
 		} else {
 			int firstThousand = (int)(1000 * 0.2);
 			int remaining = weight - 1000;
 			int remainingCharge = (int)(remaining * 0.1);
-			return firstThousand + remainingCharge;
+			return firstThousand + remainingCharge; 
 		}
 	}
 }
