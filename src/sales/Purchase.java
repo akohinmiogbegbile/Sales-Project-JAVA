c52130d3dbc2f68e3b99daae0381f2e9dfa24b2d
inmiakoh <akohinmi90@gmail.com>
2025-11-20 22:41:12 +0000

    day2 Refund

--- src/sales/Purchase.java
+++ src/sales/Purchase.java
@@ -1,5 +1,25 @@
 package sales;
 
-public class Purchase {
-
+//inheritance in action since purchase is a child class of transaction
+public class Purchase extends Transaction{
+	
+	private Saleable item;
+	private int quantity;
+	
+	public Purchase(Saleable item, int quantity) {
+		//NB - the "super" calls the transaction constructor
+		//so a product has a price which is then multiplied by the quantity which then becomes 
+		//the transaction value which is then stored in the transaction superclass
+		super(item.getPrice() * quantity); 
+		this.item = item;
+		this.quantity = quantity;
+	}
+	
+	public Saleable getItem() {
+		return item;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
 }
