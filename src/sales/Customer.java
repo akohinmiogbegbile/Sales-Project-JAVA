6bbca43f28bb5c27396040f00471833564340c15
inmiakoh <akohinmi90@gmail.com>
2025-11-22 17:52:01 +0000

    day3 Transactions

--- src/sales/Customer.java
+++ src/sales/Customer.java
@@ -1,5 +1,27 @@
 package sales;
 
-public class Customer {
+import java.util.ArrayList;
 
+public class Customer {
+	
+	private ArrayList<Transaction> transactions;
+	
+	public Customer() {
+		this.transactions = new ArrayList<>();
+	}
+	
+	public void transact(Transaction t) {
+		transactions.add(t);
+	}
+	
+	public int getTotal() {
+		int total = 0;
+		for (Transaction t: transactions) {
+			total += t.getValue();
+		}
+		return total;
+	}
+	public ArrayList<Transaction>getTransactions() {
+		return transactions;
+	}
 }
