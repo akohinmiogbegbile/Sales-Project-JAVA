0506258d544ee7b30b997b904bf477e4280e568f
inmiakoh <akohinmi90@gmail.com>
2025-12-11 19:52:10 +0000

    added a better main file

--- src/sales/Main.java
+++ src/sales/Main.java
@@ -7,20 +7,33 @@ public class Main {
 		Customer c = new Customer();
 		
 		Product efootballPlaque = new Product (80000, 1500);
-		Product xboxController = new Product (500, 50);
+		Product xboxController = new Product (5000, 275);
 		
 		Service repair = new Service(3000);
+		Refund refundController = new Refund(5000, "Returned xBoxController");
+		c.transact(refundController);
+		
 		
 		c.transact(new Purchase(efootballPlaque, 1));
 		c.transact(new Purchase(xboxController, 1));
 		c.transact(new Purchase(repair, 1));
 		
-		c.transact(new Refund(500, "Returned xboxController"));
+		//c.transact(new Refund(500, "Returned xboxController"));
 		
-		System.out.println("Customer total: £" + (c.getTotal()/100.0)); //change the value from pennies to pound
-		System.out.println("Efootball Plaque delivery: £" + efootballPlaque.calculateDelivery()/100.0);
+		System.out.println("----- TRANSACTIONS -----");
+		//divided by 100 to change the printed version from pennies to pound
+		System.out.println("Items Purchased: eFootball Plaque (£" + (efootballPlaque.getPrice()/100.0) + ")");
+		System.out.println("Items Purchased: Xbox Controller (£" + (xboxController.getPrice()/100.0) + ")");
+		System.out.println("Items Purchased: Repair Service (£" + (repair.getPrice()/100.0) + ")");
+		//System.out.println("Items Refunded: Xbox Controller (-£" + (500/100.0) + ")");
+		
+		System.out.println("Items Refunded: xBox Controller (-£" + Math.abs(refundController.getValue())/100.0 + ")");
+		System.out.println("\n----- DELIVERY COSTS -----");
+		System.out.println("eFootball Plaque delivery: £" + efootballPlaque.calculateDelivery()/100.0);
 		System.out.println("Xbox Controller delivery: £" + xboxController.calculateDelivery()/100.0);
-		
+
+		System.out.println("\n----- FINAL TOTAL -----");
+		System.out.println("Customer total: £" + c.getTotal()/100.0);
 
 	}
 
--- src/sales/Refund.java
+++ src/sales/Refund.java
@@ -21,7 +21,8 @@ public class Refund extends Transaction{
 	
 	public Refund(int value, String reason) {
 		//because a refund has to be -ve i'll multiply the value by -1 then pass it to the transaction constructor when needed.
-		super(value > 0 ? -value:value);
+		
+		super(value > 0 ? -value:value); //if value is a positive convert it to a negative and leave
 		//to store the reason for the refund
 		this.reason = reason;
 	}
